5bcf646305b0333d1c5879adcf3f436673342a53
Robsonsjre <robsonsjre@gmail.com>
2020-02-15 08:23:33 -0700

    added Router structure

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import React from 'react';
 import { useWeb3Provider, Web3Context } from './hooks/web3'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+
+const Header = () => (<div>Placeholder</div>)
+const Footer = () => (<div>Placeholder</div>)
+const HomePage = () => (<div>Placeholder</div>)
+const PageNotFound = () => (<div>Placeholder</div>)
 
 const App = () => {
   const { web3 } = useWeb3Provider()
   return (
     <Web3Context.Provider value={web3}>
-     <div>Test</div>
+     <Router>
+       <Header />
+        <Switch>
+          <Route path='/' component={HomePage} />
+          <Route component={PageNotFound} />
+        </Switch>
+        <Footer/>
+      </Router>
     </Web3Context.Provider>
   )
 }
