3a28d7ff6c4448c09b553460d613a7b47de1f5ad
Robsonsjre <robsonsjre@gmail.com>
2020-02-15 08:39:24 -0700

    added toastfy for error handling

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from "react";
 import { useWeb3Provider, Web3Context } from "./hooks/web3";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { GlobalStyles } from "./theme";
+import { toast } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 
 import HomePage from './pages/Home'
 import PageNotFound from './pages/PageNotFound'
@@ -10,6 +12,7 @@ import Header from './components/Header'
 import Footer from './components/Footer'
 
 const App = () => {
+  toast.configure()
   const { web3 } = useWeb3Provider();
   return (
     <Web3Context.Provider value={web3}>
